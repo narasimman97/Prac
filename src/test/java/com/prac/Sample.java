ca74521e770e8031ad685c6b7a6b1c2b83d3812a
narasimman97 <narasimma97gmail.com>
2022-11-07 14:48:07 +0530

    add three methods first time

---
+++ src/test/java/com/prac/Sample.java
@@ -0,0 +1,17 @@
+package com.prac;
+
+public class Sample {
+
+	public void india() {
+		System.out.println("india");
+
+	}
+	public void australia() {
+		System.out.println("aus");
+
+	}
+	public void japan() {
+	    System.out.println("jap");
+
+	}
+}
